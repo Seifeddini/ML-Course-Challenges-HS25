664cd6ba23c77b69c5762bd12612044c8f8fb82e
Siran Seifeddini <siran@seifeddini.com>
2025-10-22 10:59:24 +0200

    setup model

--- text_classification/projectExport_Challenge_-_Student_attempt_2025-10-22T08_14_50.902Z/submission.py
+++ text_classification/projectExport_Challenge_-_Student_attempt_2025-10-22T08_14_50.902Z/submission.py
@@ -3,10 +3,13 @@ import numpy as np
 import torch.nn as nn
 import torch
 from torch.utils.data import Dataset
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 
 env = {
-    "max_length": 128
+    "max_length": 128,
+    "freeze_layers": 10,
+    "lstm_hidden_size": 256,
+    "lstm_num_layers": 1
 }
 
 
@@ -33,10 +36,40 @@ def preprocess_function(examples: dict) -> dict:
     # Output: {"input_ids": [B, L], "attention_mask": [B, L]} 
     return tokenized_sample
 
+class model(nn.Module):
+    def __init__(self, num_classes=2):
+        super(model, self).__init__()
+        self.bert = BertModel.from_pretrained('bert_base_uncased')
+        for i, param in enumerate(bert.parameters()):
+            param.requires_grad = False
+            if env["freeze_layers"] >= i:
+                break
+        
+        self.bilstm = nn.LSTM(
+            input_size=768,
+            hidden_size=env["lstm_hidden_size"],
+            num_layers=env["lstm_num_layers"]
+            batch_first=True,
+            bidirectional=True
+        )
+
+        self.fc = nn.Linear(env["lstm_hidden_size"]*2, 2)
+        self.dropout = nn.Dropout(env["dropout"])
+
+    def forward(self, input_ids, attention_mask):
+        bert_outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        sequence_output = bert_outputs.last_hidden_state
+        lstm_out, _ = self.bilstm(sequence_output)
+
+        pooled_out = torch.mean(lstm_out, dim=1)
+        out = self.dropout(pooled_out)
+        logits = self.fc(out)
+        return torch.softmax(logits, dim=1)
+        
 
 # Do not change function signature
 def init_model() -> nn.Module:
-    model = ...
+    model = model()
     # Input dimension:  [B, L]   (B = batch size, L = sequence length of tokenized review)
     # Output dimension: [B, 2]   (logits for sentiment classes: negative / positive)
     return model
